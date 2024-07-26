45bc08518dad851ed8fed4590d8bac22260f273c
nahyeon109 <143698437+nahyeon109@users.noreply.github.com>
2024-07-26 15:17:39 +0900

    Title: 달에서 무게 구하기, Time: 118ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240726/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int choo = 13;
+        double biyool = 0.165;
+
+        System.out.print(choo + " * ");
+        System.out.printf("%.6f", biyool);
+        System.out.print(" = ");
+        System.out.printf("%.6f", choo*biyool);
+    }
+}
