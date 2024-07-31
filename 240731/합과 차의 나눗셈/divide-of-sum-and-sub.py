a2027056d1a1bd05a7d685dbe8236cbd222895ec
nahyeon109 <143698437+nahyeon109@users.noreply.github.com>
2024-07-31 17:41:57 +0900

    Title: 합과 차의 나눗셈, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/합과 차의 나눗셈/divide-of-sum-and-sub.py
+++ 240731/합과 차의 나눗셈/divide-of-sum-and-sub.py
@@ -3,4 +3,4 @@ a, b = input().split()
 plus_result = float(a)+float(b)
 minus_result = float(a)-float(b)
 
-print(float("{:.2f}".format(plus_result/minus_result)))
+print(round((plus_result/minus_result), 2))
