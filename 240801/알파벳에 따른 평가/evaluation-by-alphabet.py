412792930a31bcbb0780e8e3b723212e01e6f785
nahyeon109 <143698437+nahyeon109@users.noreply.github.com>
2024-08-01 15:06:37 +0900

    Title: 알파벳에 따른 평가, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240801/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+answer = input()
+
+if answer == 'S':
+    print('Superior')
+elif answer == 'A':
+    print('Excellent')
+elif answer == 'B':
+    print('Good')
+elif answer == 'C':
+    print('Usually')
+elif answer == 'D':
+    print('Effort')
+else:
+    print('Failure')
