6886ef953c1b06f82dc31040d9261d9d43dbc3f9
nahyeon109 <143698437+nahyeon109@users.noreply.github.com>
2024-08-20 19:17:01 +0900

    Title: 20대의 평균나이, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/20대의 평균나이/average-age-of-20.py
+++ 240820/20대의 평균나이/average-age-of-20.py
@@ -2,9 +2,10 @@ sum = 0
 cnt = 0
 while True:
     n = int(input())
-    if n//10 > 2:
-        print("{:.2f}".format(sum/cnt))
-        break
-    else:
+    if n//10 == 2:
         sum += n
         cnt += 1
+    else:
+        break
+    
+print("{:.2f}".format(sum/cnt))
