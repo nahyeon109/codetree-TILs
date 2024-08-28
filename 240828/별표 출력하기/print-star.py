f26be59633efaa143a82d79c39cb471c0b3e70df
nahyeon109 <143698437+nahyeon109@users.noreply.github.com>
2024-08-28 16:02:47 +0900

    Title: 별표 출력하기, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240828/별표 출력하기/print-star.py
+++ 240828/별표 출력하기/print-star.py
@@ -4,10 +4,9 @@ for i in range(n):
     for j in range(i + 1):
         print('*', end=' ')
     print()
-n -= 1
 
 
-for i in range(n, 0, -1):
+for i in range(n-1, 0, -1):
     for j in range(i, 0, -1):
         print('*', end=' ')
     print()
